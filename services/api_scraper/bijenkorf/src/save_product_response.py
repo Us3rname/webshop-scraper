94a76a14e34123fef9423168544ae7615d73a88b
Patrick Kompier <patrickkompier@gmail.com>
2021-01-31 18:49:59 +0100

    Initial commit

---
+++ services/api_scraper/bijenkorf/src/save_product_response.py
@@ -0,0 +1,233 @@
+from gql import gql, Client
+from gql.transport.aiohttp import AIOHTTPTransport
+import json
+import time
+import boto3    
+import os
+
+def store_response(event, context):
+
+    transport = AIOHTTPTransport(
+            url="https://www.debijenkorf.nl/api/graphql")
+
+    client = Client(transport=transport, fetch_schema_from_transport=True)
+    query = """
+            query { productListing(query: %s, locale: "nl-NL") { 
+            metaInformation {
+                
+            query
+            title
+            alternativeLanguages {
+                
+            locale
+            url
+
+            }
+
+            }
+            navigation {
+                products {
+                    
+            brand {
+                
+            name
+
+            }
+            code
+            colorCount
+            currentVariantProduct {
+                
+            availability {
+                
+            available
+            availableFuture
+            stock
+
+            }
+            code
+            color
+            current
+            images {
+                
+            position
+            type
+            url
+
+            }
+            overriddenPrices {
+                
+            currencyCode
+            type
+            value
+
+            }
+            sellingPrice {
+                
+            currencyCode
+            type
+            value
+
+            }
+            signings {
+                
+            discount {
+                
+            key
+            text
+
+            }
+            merchandise {
+                
+            key
+            text
+
+            }
+
+            }
+            trackingMetadata
+            size
+            url
+
+            }
+            defaultVariantCode
+            description
+            designer
+            displayName
+            displayProperties {
+                
+            currentVariantSelected
+            detailPageVariation
+
+            }
+            gift
+            name
+            subBrand {
+                
+            name
+
+            }
+            supplierModel
+            sustainable
+            trackingMetadata
+            url
+            variantProducts (limit:4, groupBy: COLOR) {
+                
+            availability {
+                
+            available
+            availableFuture
+            stock
+
+            }
+            code
+            color
+            current
+            images {
+                
+            position
+            type
+            url
+
+            }
+            overriddenPrices {
+                
+            currencyCode
+            type
+            value
+
+            }
+            sellingPrice {
+                
+            currencyCode
+            type
+            value
+
+            }
+            signings {
+                
+            discount {
+                
+            key
+            text
+
+            }
+            merchandise {
+                
+            key
+            text
+
+            }
+
+            }
+            size
+            url
+
+            }
+            recommendationRanking
+
+                }
+                pagination {
+                    
+            currentPage {
+                
+            pageNumber
+            query
+            relativeUrl
+
+            }
+            nextPage {
+                
+            pageNumber
+            query
+            relativeUrl
+
+            }
+            previousPage {
+                
+            pageNumber
+            query
+            relativeUrl
+
+            }
+            totalItemCount
+            viewSize
+
+                }
+            }
+        } }
+        """
+
+    start_index = 0
+    s3 = boto3.resource('s3')
+    next_page_query = None
+    folder_name = time.strftime("%Y%m%d-%H%M%S")
+    while next_page_query is not None or start_index == 0:
+        
+        # Prepare & execute query
+        query_addition = "fh_location=//catalog01/nl_NL/categories<{catalog01_80}/categories<{catalog01_80_1040}" + "&fh_start_index={}&country=NL&chl=1&language=nl&fh_view_size={}".format(start_index, 50)
+        qpl = gql(query % ('"' + query_addition + '"'))
+        result = client.execute(qpl)
+        
+        # Count the amount of products
+        products = result['productListing']['navigation']['products']
+        next_page_query = None
+        if result['productListing']['navigation']['pagination']['nextPage'] is not None:
+            next_page_query = result['productListing']['navigation']['pagination']['nextPage']['query']
+
+        s3object = s3.Object(os.environ['s3ResponseBucketName'], '{}/products/response - {} - {} - {}.json'.format(folder_name,time.strftime("%Y%m%d-%H%M%S"), start_index, start_index + len(products)))
+
+        s3object.put(
+            Body=(bytes(json.dumps(result).encode('UTF-8')))
+        )
+
+        start_index += 50
+
+    response = {
+        "statusCode": 200,
+        "body": "okidoki"
+    }
+
+    return response
+
+if __name__ == "__main__":
+    store_response('', '')
