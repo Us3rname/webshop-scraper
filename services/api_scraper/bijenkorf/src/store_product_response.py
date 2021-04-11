d055930618ee8afd695a7d5aa641165b8dd1f642
Patrick Kompier <patrickkompier@gmail.com>
2021-04-11 21:13:38 +0200

    Store bijenkorf response + daily schedule

--- services/api_scraper/bijenkorf/src/store_product_response.py
+++ services/api_scraper/bijenkorf/src/store_product_response.py
@@ -199,8 +199,14 @@ def store_response(event, context):
 
     start_index = 0
     s3 = boto3.resource('s3')
+    sqs = boto3.resource('sqs')
+
+    queue = sqs.get_queue_by_name(QueueName="dev-webshop-scraper-infra-webscraper-BijenkorfQueue0CAC392F-ALBMVIY0WXN9")
+
     next_page_query = None
     folder_name = time.strftime("%Y%m%d-%H%M%S")
+
+    # Iterate at least one time (start_index == 0) and continue till there are no next pages left.
     while next_page_query is not None or start_index == 0:
         
         # Prepare & execute query
@@ -208,17 +214,29 @@ def store_response(event, context):
         qpl = gql(query % ('"' + query_addition + '"'))
         result = client.execute(qpl)
         
-        # Count the amount of products
+        # Get all the products
         products = result['productListing']['navigation']['products']
         next_page_query = None
         if result['productListing']['navigation']['pagination']['nextPage'] is not None:
             next_page_query = result['productListing']['navigation']['pagination']['nextPage']['query']
 
-        s3object = s3.Object(os.environ['s3ResponseBucketName'], '{}/products/response - {} - {} - {}.json'.format(folder_name,time.strftime("%Y%m%d-%H%M%S"), start_index, start_index + len(products)))
+        fileLocation =  'products/{}/response - {} - {} - {}.json'.format(folder_name,time.strftime("%Y%m%d-%H%M%S"), start_index, start_index + len(products))
+        s3object = s3.Object(os.environ['s3ResponseBucketName'], fileLocation)
 
         s3object.put(
             Body=(bytes(json.dumps(result).encode('UTF-8')))
         )
+
+        response = queue.send_message(MessageBody="Producten {} t/m {}".format(start_index, start_index + len(products)), MessageAttributes = {
+            'ProductS3Location': {
+                'StringValue': fileLocation,
+                'DataType': 'String'
+            },
+            'FolderName': {
+                'StringValue': folder_name,
+                'DataType': 'String'
+            }
+        })
 
         start_index += 50
 
